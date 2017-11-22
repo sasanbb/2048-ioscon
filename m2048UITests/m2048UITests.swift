b55273ea0260985a97a587cab096ca667690934f
Sasan Naderi <sasan@buddybuild.com>
2017-11-22 14:25:52 -0800

    Update SDK.

--- m2048UITests/m2048UITests.swift
+++ m2048UITests/m2048UITests.swift
@@ -21,12 +21,12 @@ class m2048UITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-        BuddyBuildSDK.startUITestsVideoRecording()
+        BuddyBuildSDK.startUITests()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        BuddyBuildSDK.stopUITestsVideoRecording()
+        BuddyBuildSDK.stopUITests()
         
         super.tearDown()
     }
